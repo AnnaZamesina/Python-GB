84f2e24cb32641a2fa749c2c7db8d49da7047250
Anna Zamesina <zamesinaanna@gmail.com>
2023-02-20 11:38:19 +0300

    tasks 34 36

--- Desktop/Python/task036.py
+++ Desktop/Python/task036.py
@@ -7,7 +7,9 @@
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
     for i in range(1, num_rows+1):
-            print(*(operation(i, j) for j in range(1, num_columns+1)))
-
+        res=list()
+        for j in range(1, num_columns+1):
+            res.append((operation(i, j)))
+        print("\n",res)
 
 print_operation_table(lambda x, y: x * y)
