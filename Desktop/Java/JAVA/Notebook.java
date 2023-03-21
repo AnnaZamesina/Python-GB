9d8a6dfc4f4a815ef022c7aeb83c67161dcba6e6
Anna Zamesina <zamesinaanna@gmail.com>
2023-03-21 20:16:07 +0300

    restart

---
+++ Desktop/Java/JAVA/Notebook.java
@@ -0,0 +1,68 @@
+package JAVA;
+
+public class Notebook {
+    private String model;
+    private Integer ram;
+    private Integer hard_drive_volume;
+    private String operating_system;
+    private String color;
+    private Integer price;
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Модель: %s, Количество оперативной памяти: %d, Жесткий диск: %d, Операционная система: %s, Цвет: %s, Цена: %d",
+                model, ram, hard_drive_volume, operating_system, color, price);
+    }
+
+    public Notebook(String model, Integer ram, Integer hard_drive_volume, String operating_system, String color,
+            Integer price) {
+        this.model = model;
+        this.ram = ram;
+        this.hard_drive_volume = hard_drive_volume;
+        this.operating_system = operating_system;
+        this.color = color;
+        this.price = price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getRAM() {
+        return ram;
+    }
+
+    public void setRAM(int ram) {
+        this.ram = ram;
+    }
+
+    public String getOperatingSystem() {
+        return operating_system;
+    }
+
+    public void setOperatingSystem(String operating_system) {
+        this.operating_system = operating_system;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
---
+++ Desktop/Java/main.java
@@ -0,0 +1,12 @@
+import JAVA.Notebook;
+
+public class main {
+    public static void name() {
+        Notebook notebook1 = new Notebook("Dell", 8, 8, "Windows", "grey", 100);
+        Notebook notebook2 = new Notebook("Mac", 16, 16, "MAC OS", "white", 1000);
+        Notebook notebook3 = new Notebook("Samsung", 32, 128, "Windows", "black", 150);
+        Notebook notebook4 = new Notebook("Honor", 64, 64, "Linux", "white", 300);
+        Notebook notebook5 = new Notebook("Huawei", 128, 64, "Linux", "red", 700);
+        System.out.println(notebook1);
+    }
+}
